1dfa7963921cef531641bc106e43f6177f3d9a73
Pedro Vitor <pedro.git@teste.com>
2026-05-31 02:10:27 -0300

    feat(shell): expose app launcher to QML

--- packages/ped-shell/src/main.cpp
+++ packages/ped-shell/src/main.cpp
@@ -1,20 +1,26 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QUrl>
+
 #include "systeminfo.h"
 #include "gamemode.h"
 #include "applauncher.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QGuiApplication app(argc, argv);
 
-  SystemInfo systemInfo;
-GameMode gameMode;
-AppLauncher appLauncher;
+    QQmlApplicationEngine engine;
 
-engine.rootContext()->setContextProperty("systemInfo", &systemInfo);
-engine.rootContext()->setContextProperty("gameMode", &gameMode);
-engine.rootContext()->setContextProperty("appLauncher", &appLauncher);
+    SystemInfo systemInfo;
+    GameMode gameMode;
+    AppLauncher appLauncher;
+
+    engine.rootContext()->setContextProperty("systemInfo", &systemInfo);
+    engine.rootContext()->setContextProperty("gameMode", &gameMode);
+    engine.rootContext()->setContextProperty("appLauncher", &appLauncher);
+
     engine.load(QUrl(QStringLiteral("qrc:/PedShell/qml/Main.qml")));
 
     if (engine.rootObjects().isEmpty())
